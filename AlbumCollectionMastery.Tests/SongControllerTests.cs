a43437c00bb69ec7af8f092cf5146e668bd926bb
Joe Scheiman <scheiman.joe@gmail.com>
2019-03-25 01:37:58 -0400

    Completed minimum tests for full credit - Used Received from NSub

--- AlbumCollectionMastery.Tests/SongControllerTests.cs
+++ AlbumCollectionMastery.Tests/SongControllerTests.cs
@@ -11,17 +11,40 @@ namespace AlbumCollectionMastery.Tests
 {
     public class SongControllerTests
     {
+        public SongController DUT;
+        public ISongRepository testRepo;
+
+        public SongControllerTests()
+        {
+            testRepo = Substitute.For<ISongRepository>();
+            DUT = new SongController(testRepo);
+        }
+
         [Fact]
-        public void Can_Create_a_Song()
+        public void Create_a_Song_Returns_a_View()
         {
             var expectedId = 3;
-            var testRepo = new SongRepository(null);
-            var DUT = new SongController(testRepo);
-
             var testResult = DUT.Create(expectedId);
 
             Assert.IsType<ViewResult>(testResult);
 
+        }
+
+        [Fact]
+        public void Can_Create_a_Song()
+        {
+            var pass = true;
+
+            var newSong = new Song();
+            var wrongSong = new Song();
+            //This example is based off NSubstitute website,
+            //No Assert is needed.
+            testRepo.Create(newSong);
+            if(!pass) //Change var pass to false to see this fail
+                testRepo.Received().Create(wrongSong);            
+            
+            if(pass)
+                testRepo.Received().Create(newSong);
         }
     }
 }
